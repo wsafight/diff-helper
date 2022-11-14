087c05a67730eef7f1ca1b83012e14e804044e24
wsafight <984292420@qq.com>
2022-11-14 13:15:38 +0800

    #STYLE: code change

--- src/common.ts
+++ src/common.ts
@@ -1,69 +1,69 @@
 import { EmptyVal, getOwnKeysForObj, isDate, isEmpty, isObject, isRealObject } from "./utils"
 
 const _coreDiff = (newVal: any, oldVal: any,  empty: EmptyVal = null): any => {
-	if (oldVal === undefined || oldVal === '') {
-		oldVal = null
-	}
-	if (newVal === undefined || newVal === '') {
-		newVal = null
-	}
+  if (oldVal === undefined || oldVal === '') {
+    oldVal = null
+  }
+  if (newVal === undefined || newVal === '') {
+    newVal = null
+  }
 
-	if (oldVal === newVal) {
-		return null
-	}
+  if (oldVal === newVal) {
+    return null
+  }
 
-	if (!isObject(oldVal) || !isObject(newVal)) {
-		return newVal
-	}
+  if (!isObject(oldVal) || !isObject(newVal)) {
+    return newVal
+  }
 
-	if (isDate(oldVal) || isDate(newVal)) {
-		if (oldVal.valueOf() === newVal.valueOf()) {
-			return null
-		}
-		return newVal
-	}
+  if (isDate(oldVal) || isDate(newVal)) {
+    if (oldVal.valueOf() === newVal.valueOf()) {
+      return null
+    }
+    return newVal
+  }
 
-	const diffResult: Record<string, any> = {}
+  const diffResult: Record<string, any> = {}
 
-	const checkedKeys: Set<string> = new Set<string>();
+  const checkedKeys: Set<string> = new Set<string>();
 
-	Object.keys(newVal).forEach(key => {
-		if (isEmpty(newVal[key])) {
-			return
-		}
+  Object.keys(newVal).forEach(key => {
+    if (isEmpty(newVal[key])) {
+      return
+    }
 
-		checkedKeys.add(key)
+    checkedKeys.add(key)
 
-		if (isEmpty(oldVal[key])) {
-			diffResult[key] = newVal[key]
-		} else {
-			const difference = _coreDiff((oldVal as any)[key], (newVal as any)[key], empty)
-			if (difference !== null) {
-				diffResult[key] = difference
-			}
-		}
-	})
-	Object.keys(oldVal).forEach(key => {
-		if (checkedKeys.has(key)) {
-			return
-		}
-		if (!isEmpty(oldVal[key]) && isEmpty(newVal[key])) {
-			diffResult[key] = empty
-		}
-	})
-	return Object.keys(diffResult).length ? diffResult : null
+    if (isEmpty(oldVal[key])) {
+      diffResult[key] = newVal[key]
+    } else {
+      const difference = _coreDiff((oldVal as any)[key], (newVal as any)[key], empty)
+      if (difference !== null) {
+        diffResult[key] = difference
+      }
+    }
+  })
+  Object.keys(oldVal).forEach(key => {
+    if (checkedKeys.has(key)) {
+      return
+    }
+    if (!isEmpty(oldVal[key]) && isEmpty(newVal[key])) {
+      diffResult[key] = empty
+    }
+  })
+  return Object.keys(diffResult).length ? diffResult : null
 }
 
 export const coreDiff = <T>(newVal: T, oldVal: T, empty?: EmptyVal) => _coreDiff(oldVal, newVal, empty)
 
 interface simpleDiffObjOptions {
-	empty?: EmptyVal
+  empty?: EmptyVal
   diffFun?: (key: string, newVal: any, oldVal: any) => boolean
 }
 
 export const simpleObjDiff = (
   newVal: Record<string, any>,
-	oldVal: Record<string, any>,
+  oldVal: Record<string, any>,
   { 
     empty = null,
     diffFun
@@ -71,19 +71,19 @@ export const simpleObjDiff = (
     empty: null
   }
 ): Record<string, any> => {
-	if (!isRealObject(oldVal) || !isRealObject(newVal)) {
-		return {}
-	}
+  if (!isRealObject(oldVal) || !isRealObject(newVal)) {
+    return {}
+  }
 
-	const diffResult: Record<string, any> = {}
+  const diffResult: Record<string, any> = {}
 
-	const checkedKeys: Set<string> = new Set()
+  const checkedKeys: Set<string> = new Set()
 
   const hasDiffFun = typeof diffFun === 'function'
 
-	const keys = getOwnKeysForObj(newVal)
-	keys.forEach(key => {
-		checkedKeys.add(key)
+  const keys = getOwnKeysForObj(newVal)
+  keys.forEach(key => {
+    checkedKeys.add(key)
     let isChanged = false
 
     if (hasDiffFun) {
@@ -98,17 +98,24 @@ export const simpleObjDiff = (
       return
     }
 
-		if (JSON.stringify(newVal[key]) !== JSON.stringify(oldVal[key])) {
-			diffResult[key] = newVal
-		}
-	})
+    if (JSON.stringify(newVal[key]) !== JSON.stringify(oldVal[key])) {
+      diffResult[key] = newVal
+    }
+  })
 
-	const initialKeys = getOwnKeysForObj(oldVal)
-	initialKeys.forEach(key => {
-		if (checkedKeys.has(key)) {
-			return
-		}
-		diffResult[key] = empty
-	})
-	return diffResult
+  const initialKeys = getOwnKeysForObj(oldVal)
+  initialKeys.forEach(key => {
+    if (checkedKeys.has(key)) {
+      return
+    }
+    diffResult[key] = empty
+  })
+  return diffResult
+}
+
+export const simpleListDiff = <T>(
+  newVal: T[],
+  oldVal: T[]
+) => {
+  
 }
